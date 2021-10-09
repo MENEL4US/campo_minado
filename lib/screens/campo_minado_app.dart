937e0a23a04af6460ac5da36628308c652b569da
MENEL4US <luizrodriguesbmm@gmail.com>
2021-10-09 15:40:03 -0300

    Widget do campo e imagens

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
+import '../components/campo_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
-  _reiniciar() {
+  void _reiniciar() {
     print('_reiniciar...');
   }
 
+  void _abrir(Campo capo) {
+    print('_abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('_alternarMarcacao...');
+  }
+
   Widget build(BuildContext context) {
+    Campo vizinho = Campo(coluna: 1, linha: 1);
+    vizinho.minar();
+
+    Campo vizinho2 = Campo(coluna: 1, linha: 1);
+    vizinho2.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho);
+    campo.adicionarVizinho(vizinho2);
+
+    try {
+      // campo.minar();
+      campo.alternarMarcacao();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -14,7 +40,11 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
